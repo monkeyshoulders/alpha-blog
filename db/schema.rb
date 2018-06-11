02c3bde3d67d8108e71511aaaa6fcdf5aa3c752b
Matt <mattblesoe2@gmail.com>
2018-06-11 14:07:36 -0400

    Article categories db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_06_200119) do
+ActiveRecord::Schema.define(version: 2018_06_11_173752) do
+
+  create_table "article_categories", force: :cascade do |t|
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
